5dc3b3b8ca49d5be5c62289e491bef3bfbf8becb
Sasanka <kosuru.sasanka@gmail.com>
2023-04-02 12:22:30 -0700

    Login page UI

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+var cors = require('cors')
 const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('./models/User');
@@ -15,6 +16,7 @@ const app = express();
 // Set up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(session({
     secret: 'mysecret',
     resave: false,
@@ -22,6 +24,16 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set up cors
+var corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+
+app.options('*', cors(corsOptions))
 
 // Set up Passport
 passport.use(new LocalStrategy((username, password, done) => {
@@ -67,14 +79,4 @@ mongoose
     })
     .catch(e => {
         console.error('Connection error', e.message)
-    })
-
-// mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to MongoDB');
-//         // Start server
-//         app.listen(3000, () => {
-//             console.log('Server started on port 3000');
-//         });
-//     })
-//     .catch(err => console.log(err));
+    });
