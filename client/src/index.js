c1808e556b59982cc82135f61ca027a2969d18a5
Kosuru Sasanka <30541568+ksasanka11@users.noreply.github.com>
2023-04-19 13:36:22 -0700

    File APIs(upload, get and share) (#4)
    * Draft: File upload API and UI
    
    * API to get files belonging to a user
    
    * Fix whitelist apis
    
    * APIs to share files
    
    * changes to get files command

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./Components/LoginPage";
 import Dashboard from "./Components/Dashboard";
 import isAuthenticated from "./utils/isAuthenticated";
+import UploadFile from "./Components/UploadFile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -21,6 +22,7 @@ const AppRoutes = () => (
 	<Routes>
 		<Route path="/login" element={<LoginPage />} />
 		<Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
+		<Route path="/files" element={<PrivateRoute element={<UploadFile />} />} />
 	</Routes>
 );
 
